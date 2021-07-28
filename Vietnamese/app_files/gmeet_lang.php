b8b5f1ccde829d3b196cf7e910de97b9912e5341
sakevn <61232095+sakevn@users.noreply.github.com>
2021-07-28 15:04:20 +0700

    version 6.3
    Font version 6.3 and addon
    Zoom Live Classes
    Gmeet Live Classes
    Online Course

---
+++ Vietnamese/app_files/gmeet_lang.php
@@ -0,0 +1,23 @@
+<?php 
+$lang['api_key'] ="Mã API";
+$lang['api_secret'] ="Bí mật API";
+$lang['cancelled'] ="Đã hủy";
+$lang['class_date'] ="Ngày học";
+$lang['class_duration_minutes'] ="Thời lượng lớp học (phút)";
+$lang['class_title'] ="Tên lớp";
+$lang['created_for'] ="Tạo cho";
+$lang['finished'] ="Đã kết thúc";
+$lang['gmeet'] ="Gmeet";
+$lang['gmeet_live_classes'] ="Lớp học trực tiếp Gmeet";
+$lang['gmeet_online_classes'] ="Lớp học trực tiếp Gmeet";
+$lang['gmeet_online_classes_start'] ="Lớp học trực tiếp Gmeet bắt đầu";
+$lang['gmeet_online_meeting'] ="Cuộc họp trực tiếp Gmeet";
+$lang['gmeet_online_meeting_start'] ="Bắt đầu cuộc họp trực tiếp Gmeet";
+$lang['how_to_get'] ="Làm thế nào để có được";
+$lang['invited'] ="Được mời";
+$lang['join'] ="Tham gia";
+$lang['live_class'] ="Lớp học trực tiếp";
+$lang['live_meeting'] ="Họp trực tiếp";
+$lang['meeting_duration_minutes'] ="Thời lượng cuộc họp (phút)";
+$lang['setting'] ="Cài đặt";
+$lang['use_google_api'] ="Sử dụng Google Api";
